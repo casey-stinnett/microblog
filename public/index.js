4e65cc28d40d3c8895b761d7db30e112ed4bb970
Casey Stinnett <casey.stinnett@nycda.com>
2016-09-16 14:36:14 -0600

    Functionality for sign-in, sign-out, view/edit profile

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 $(function(){
-	$("#show-pass").click(function(){
+	$('.show-pass').on('click', function(){
 		$(this).toggleClass('fa-eye fa-eye-slash');
-		$input = $(this).prev('input');
+		$input = $(this).parent('span').prev('input');
 
 		if ($(this).hasClass('fa-eye-slash')) {
 			$input.attr('type', 'text');
@@ -11,4 +11,29 @@ $(function(){
 			$(this).attr('title', 'Show Password');
 		}
 	});
+
+	$('input[type=password]').on('keyup', function(){
+		if ($(this).val() != '') {
+			$(this).next('span').show();
+		} else {
+			$(this).next('span').hide();
+		}
+	});
+
+	$('#post-content').on('focus', function(){
+		$(this).removeClass('content-no-focus').addClass('content-focus');
+	})
+	.on('keyup', function(){
+		if ($(this).val() != '') {
+			$('#post-button').prop('disabled', false).removeClass('disabled');
+			$(this).addClass('content-typing');
+		} else {
+			$('#post-button').prop('disabled', true).addClass('disabled');
+			$(this).removeClass('content-typing');
+		}
+	})
+	.parent('.post-box').on('focusout', function(){
+		$(this).find('#post-content').removeClass('content-focus').addClass('content-no-focus');
+	});
+	
 });
